6f98ad4899bac0a150a92e20a76a1d4164b6508d
Perry <chenzhipeng901105@gmail.com>
2015-11-27 18:58:39 +0800

    add

--- Grapevine/Grapevine/ViewController.swift
+++ Grapevine/Grapevine/ViewController.swift
@@ -35,5 +35,59 @@ class ViewController: UIViewController {
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+//    appImageView.hidden = true
+//    welcomeLabel.hidden = true
+//    summaryLabel.hidden = true
+//    pageControl.hidden = true
+    
+    // 1
+    // Create a views dictionary that holds string representations of views to resolve inside the format string.
+    let views = ["iconImageView": iconImageView,
+    "appNameLabel": appNameLabel,
+    "skipButton": skipButton,
+    "appImageView": appImageView,
+    "welcomeLabel": welcomeLabel,
+    "summaryLabel": summaryLabel,
+    "pageControl": pageControl]
+    
+    // 2
+    // Create a mutable array of constraints. You'll build this up in the rest of the code.
+    var allConstraints = [NSLayoutConstraint]()
+    
+    // 3
+    // set up vertical constraints for the iconImageView, placing its top edge 20 points from its superview's top edge, with a height of 30 points.
+    let iconVerticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[iconImageView(30)]", options: [], metrics: nil, views: views)
+    allConstraints += iconVerticalConstraints
+    
+    // 4
+    // Set up vertical constraints for the appNameLabel, placing its top edge 23 points from its superview's top edge.
+    let nameLabelVerticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-23-[appNameLabel]", options: [], metrics: nil, views: views)
+    allConstraints += nameLabelVerticalConstraints
+    
+    // 5
+    // Set up vertical constraints for the skipButton, placing its top edge 20 points from its superview's top edge.
+    let skipButtonVerticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[skipButton]", options: [], metrics: nil, views: views)
+    allConstraints += skipButtonVerticalConstraints
+    
+    // 6
+    // set horizontal constraint
+    let topRowHorizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-15-[iconImageView(30)]-[appNameLabel]-[skipButton]-15-|", options: [], metrics: nil, views: views)
+    allConstraints += topRowHorizontalConstraints
+    
+    // 1
+    let summaryHorizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-15-[summaryLabel]-15-|", options: [], metrics: nil, views: views)
+    allConstraints += summaryHorizontalConstraints
+    
+    let welcomeHorizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-15-[welcomeLabel]-15-|", options: [], metrics: nil, views: views)
+    allConstraints += welcomeHorizontalConstraints
+    
+    // 2
+//    let iconToImageVertical
+    
+    // 7
+    // activate the layout constraints
+    NSLayoutConstraint.activateConstraints(allConstraints)
+    
   }
 }
